fa40acda8b05ea2e985e7985793d03c3c3321ae8
talaalosili <talaalosili@gmail.com>
2024-06-27 07:55:45 +0300

    tala

---
+++ main.js
@@ -0,0 +1,21 @@
+//task1
+// var headerElement = document.getElementById("header");
+// headerElement.innerHTML = "Welcome to DOM Manipulation!";
+//task2
+// const myElement = document.getElementsByClassName("item");
+// myElement[0].innerHTML = "Class Item";
+//task3
+// const paragraphs = document.getElementsByName("p");
+// paragraphs[0].innerHTML = "Paragraph Elemebt";
+//task4
+// const firstHighlight = document.querySelector(".highlight");
+// firstHighlight.textContent = "First Highlighted Item";
+
+//task5
+// const listItems = document.querySelectorAll("li");
+// if (listItems.length > 0) {
+//     listItems.forEach(item => {
+//         item.textContent = "List Item";
+//     });
+// }
+//task6
